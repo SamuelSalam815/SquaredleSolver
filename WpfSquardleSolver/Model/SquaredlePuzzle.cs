9e8f9a371c4535f4f9bf7e88a37c18a671308f76
sam <samuelsalam815@gmail.com>
2023-12-19 20:50:12 +0000

    add skeleton squardle puzzle model and view model

---
+++ WpfSquardleSolver/Model/SquaredlePuzzle.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WpfSquardleSolver.Model;
+
+class SquaredlePuzzle
+{
+    public List<string> ValidWords { get; set; }
+    public string PuzzleAsText { get; set; }
+}
---
+++ WpfSquardleSolver/ViewModel/SquaredleViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using WpfSquardleSolver.Model;
+
+namespace WpfSquardleSolver.ViewModel;
+
+class SquaredleViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private bool isPuzzleBeingSolved;
+    private readonly SquaredlePuzzle puzzleModel;
+
+    public SquaredleViewModel()
+    {
+        this.puzzleModel = new();
+    }
+
+    public bool IsPuzzleBeingSolved
+    {
+        get { return isPuzzleBeingSolved; }
+        set
+        {
+            isPuzzleBeingSolved = value;
+            OnPropertyChanged(nameof(IsPuzzleBeingSolved));
+        }
+    }
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public BindingList<string> WordsFoundInPuzzle { get; } = new();
+
+    private void TogglePuzzleSolverOnOff(object? parameter)
+    {
+    }
+
+    private bool CanTogglePuzzleSolverOnOff(object? parameter)
+    {
+        return true;
+    }
+
+    public void SetPuzzleText()
+    {
+        if (IsPuzzleBeingSolved)
+        {
+            TogglePuzzleSolverOnOff(null);
+        }
+    }
+
+    public void LoadValidWords(string path)
+    {
+    }
+}
