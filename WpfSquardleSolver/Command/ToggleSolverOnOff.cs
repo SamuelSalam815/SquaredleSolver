b2f5f48ec1edbc6c51e31f15be05353d55ad5cfe
sam <samuelsalam815@gmail.com>
2023-12-20 21:29:15 +0000

    Move ToggleSolverCommand to appropriately named namespace

---
+++ WpfSquardleSolver/Command/ToggleSolverOnOff.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+using WpfSquardleSolver.Model;
+
+namespace WpfSquardleSolver.Command;
+
+class ToggleSolverOnOff : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+    private readonly SolverModel solver;
+
+    public ToggleSolverOnOff(SolverModel solver)
+    {
+        this.solver = solver;
+    }
+
+    public bool CanExecute(object? parameter) => true;
+
+    public void Execute(object? parameter)
+    {
+        if (solver.IsSolverRunning)
+        {
+            solver.StopSolvingPuzzle();
+        }
+        else
+        {
+            solver.StartSolvingPuzzle();
+        }
+    }
+}
