e4c8e10e09efa415380cf1aa48282c5ea5190b34
sam <samuelsalam815@gmail.com>
2023-12-20 21:40:18 +0000

    Remove redundant code from code-behind of main window

--- WpfSquardleSolver/View/MainWindow.xaml.cs
+++ WpfSquardleSolver/View/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
+using WpfSquardleSolver.Command;
+using WpfSquardleSolver.Model;
 
 namespace WpfSquardleSolver;
 /// <summary>
@@ -11,24 +9,17 @@ namespace WpfSquardleSolver;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private PuzzleSolver puzzleSolver;
+    private readonly SolverModel solver;
+    private readonly PuzzleModel puzzle;
+    public ICommand ToggleSolverOnOff;
 
     public MainWindow()
     {
+        puzzle = new PuzzleModel();
+        puzzle.LoadValidWords("words_alpha.txt");
+        solver = new SolverModel(puzzle);
+        ToggleSolverOnOff = new ToggleSolverOnOff(solver);
         InitializeComponent();
-        HashSet<string> allowedWords = new();
-
-        using StreamReader reader = new("words_alpha.txt");
-        string? line = reader.ReadLine();
-        while (line is not null)
-        {
-            allowedWords.Add(line.ToUpper());
-            line = reader.ReadLine();
-        }
-
-        puzzleSolver = new(allowedWords);
-        ResultsListView.ItemsSource = puzzleSolver.Answers;
-        puzzleSolver.PropertyChanged += OnPuzzleSolverStatusChanged;
     }
 
     private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -52,36 +43,4 @@ public partial class MainWindow : Window
         e.Handled = true;
     }
 
-    private void OnPuzzleSolverStatusChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        Application.Current.Dispatcher.Invoke(() =>
-        {
-            if (puzzleSolver.IsSolverRunning)
-            {
-                ToggleSolverButton.Content = "Stop Solving Puzzle";
-                ToggleSolverButton.Background = new BrushConverter().ConvertFrom("#e74c3c") as SolidColorBrush;
-            }
-            else
-            {
-                ToggleSolverButton.Content = "Start Solving Puzzle";
-                ToggleSolverButton.Background = new BrushConverter().ConvertFrom("#2ecc71") as SolidColorBrush;
-            }
-        });
-    }
-
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
-        if (sender == ToggleSolverButton)
-        {
-            if (puzzleSolver.IsSolverRunning)
-            {
-                puzzleSolver.StopSolvingPuzzle();
-            }
-            else
-            {
-                puzzleSolver.UpdatePuzzle(InputField.Text);
-                puzzleSolver.StartSolvingPuzzle();
-            }
-        }
-    }
 }
