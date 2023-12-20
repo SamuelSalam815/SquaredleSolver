e804077563376a0d81a08f57c35f818f4848d753
sam <samuelsalam815@gmail.com>
2023-12-20 21:01:25 +0000

    Remove skeleton model classes

--- WpfSquardleSolver/Model/PuzzleModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WpfSquardleSolver.Model;
-
-class PuzzleModel
-{
-    public HashSet<string> ValidWords { get; set; } = new();
-    public string PuzzleAsText { get; set; } = "";
-}
--- WpfSquardleSolver/Model/SolverModel.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace WpfSquardleSolver.Model;
-class SolverModel
-{
-    public BindingList<string> ValidWordsFoundInPuzzle { get; } = new();
-}
