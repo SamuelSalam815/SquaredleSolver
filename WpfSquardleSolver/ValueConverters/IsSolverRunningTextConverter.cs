49a8b73593f7967d846ac9a5af706927f00eea7a
sam <samuelsalam815@gmail.com>
2023-12-26 13:18:34 +0000

    Make button text change depending on whether the puzzle is being solved or not

---
+++ WpfSquardleSolver/ValueConverters/IsSolverRunningTextConverter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfSquardleSolver.ValueConverters;
+
+/// <summary>
+///     Determines the text for the button that toggles the puzzle solver on or off
+/// </summary>
+internal class IsSolverRunningTextConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not bool boolValue)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        if (boolValue)
+        {
+            return "Stop Solving Puzzle";
+        }
+
+        return "Start Solving Puzzle";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
